d531f992f7aca993965077ecbe7f47925a623fae
Jan Slifka <jan.slifka@gmail.com>
2023-10-03 12:25:38 +0200

    3.28.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project_name = 'DSW'
 project_name_full = 'Data Stewardship Wizard'
 
 # The full version, including alpha/beta/rc tags
-version = release = '3.27'
+version = release = '3.28'
 
 rst_prolog = f"""
 
