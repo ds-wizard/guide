a2ab48d708484ff43e453529ede44132bcdabff9
krystofkomanec <krystof.komanec@gmail.com>
2025-06-02 16:42:36 +0300

    Add better 404 page

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx_toolbox.confval',
     'sphinx.ext.imgconverter',
+    'notfound.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
