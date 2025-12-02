ed7a171282c62ba9227ef4bd5f8e5c87a939fb60
krystofkomanec <krystof.komanec@gmail.com>
2025-12-02 12:26:42 +0100

    4.25.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ project_name = 'DSW'
 project_name_full = 'Data Stewardship Wizard'
 
 # The full version, including alpha/beta/rc tags
-version = release = '4.24'
+version = release = '4.25'
 
 rst_prolog = f"""
 
